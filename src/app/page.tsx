67816713eeddc3e8218f2eeee91183384af5b3a4
Victor Cornejo <rex2002xp@gmail.com>
2024-12-26 19:19:06 -0600

    chore: Formateo de codigo con Prettier.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,27 @@
-import { SidebarTrigger } from "@/components/ui/sidebar";
-import { Separator } from "@/components/ui/separator";
+import { SidebarTrigger } from '@/components/ui/sidebar';
+import { Separator } from '@/components/ui/separator';
 import {
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
-  BreadcrumbList, BreadcrumbPage,
-  BreadcrumbSeparator
-} from "@/components/ui/breadcrumb";
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
 
 export default function Home() {
   return (
     <>
-      <header
-        className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+      <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
         <div className="flex items-center gap-2 px-4">
-          <SidebarTrigger className="-ml-1"/>
-          <Separator orientation="vertical" className="mr-2 h-4"/>
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
           <Breadcrumb>
             <BreadcrumbList>
               <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">
-                  Consola Administrativa
-                </BreadcrumbLink>
+                <BreadcrumbLink href="#">Consola Administrativa</BreadcrumbLink>
               </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block"/>
+              <BreadcrumbSeparator className="hidden md:block" />
               <BreadcrumbItem>
                 <BreadcrumbPage>Dashboard</BreadcrumbPage>
               </BreadcrumbItem>
@@ -31,15 +29,14 @@ export default function Home() {
           </Breadcrumb>
         </div>
       </header>
-      <div className="w-[calc(100%)] flex flex-1 flex-col gap-4 p-4  bg-amber-100">
+      <div className="flex w-[calc(100%)] flex-1 flex-col gap-4 bg-amber-100 p-4">
         <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div className="aspect-video rounded-xl bg-muted/50"/>
-          <div className="aspect-video rounded-xl bg-muted/50"/>
-          <div className="aspect-video rounded-xl bg-muted/50"/>
-          <div className="aspect-video rounded-xl bg-muted/50"/>
+          <div className="aspect-video rounded-xl bg-muted/50" />
+          <div className="aspect-video rounded-xl bg-muted/50" />
+          <div className="aspect-video rounded-xl bg-muted/50" />
+          <div className="aspect-video rounded-xl bg-muted/50" />
         </div>
       </div>
     </>
-  )
-    ;
+  );
 }
