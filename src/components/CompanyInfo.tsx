6df864a53fe966f90405d0b8edae376f5d0fead3
Victor Cornejo <rex2002xp@gmail.com>
2025-01-27 14:28:18 -0600

    fix: Se aplicaron correcciones sugeridas por Sonarqube. (#5)

--- src/components/CompanyInfo.tsx
+++ src/components/CompanyInfo.tsx
@@ -6,16 +6,14 @@ export function CompanyInfo() {
   const applicationName = env.NEXT_PUBLIC_APPLICATION_NAME;
   const companyName = env.NEXT_PUBLIC_COMPANY_NAME;
   return (
-    <>
-      <div className="flex flex-row gap-4">
-        <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-          <ShieldCheck className="size-4" />
-        </div>
-        <div className="grid flex-1 text-left text-sm leading-tight">
-          <span className="truncate font-semibold">{applicationName}</span>
-          <span className="truncate text-xs">{companyName}</span>
-        </div>
+    <div className="flex flex-row gap-4">
+      <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+        <ShieldCheck className="size-4" />
       </div>
-    </>
+      <div className="grid flex-1 text-left text-sm leading-tight">
+        <span className="truncate font-semibold">{applicationName}</span>
+        <span className="truncate text-xs">{companyName}</span>
+      </div>
+    </div>
   );
 }
